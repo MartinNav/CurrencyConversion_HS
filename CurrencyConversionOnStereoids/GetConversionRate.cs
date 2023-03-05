99df1da53666aed4eaf30e35e06d1dacf870b36f
MartinNav <94181420+MartinNav@users.noreply.github.com>
2023-03-05 18:14:08 +0100

    Added code

---
+++ CurrencyConversionOnStereoids/GetConversionRate.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+namespace CurrencyConversionOnStereoids
+{
+    class GetConversionRate
+    {
+        string currencyFrom;
+        string currencyTo;
+        decimal rate;
+        public GetConversionRate(string from, string to)
+        {
+            this.currencyFrom = from.ToUpper();
+            this.currencyTo = to.ToUpper();
+        }
+        public bool LoadRate()
+        {
+            try
+            {
+
+            string resp = string.Empty;
+            string url = @"https://open.er-api.com/v6/latest/" + currencyFrom;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                resp = reader.ReadToEnd();
+            }
+                // Console.WriteLine(resp);//Have to delete this debug info and add errror handling
+
+
+
+                string dat;
+                if (resp!=null)
+                {
+                   dat = JObject.Parse(resp)["rates"].ToString();
+
+                }
+                else
+                {
+                    System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                    dat = "0.0";
+                }
+
+
+
+
+                //Console.WriteLine(dat);
+                string curr;
+                 if (dat!=null)
+                 {
+                     curr = JObject.Parse(dat)[currencyTo].ToString();
+                 }
+                else
+                {
+                    System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                    curr = "0.0";
+                }
+            //Console.WriteLine(curr);
+            rate = decimal.Parse(curr);
+            return true;
+            }
+            catch
+            {
+                System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                return false;
+            }
+
+        }
+        public decimal GetRate()
+        {
+            return rate;
+        }
+    }
+}
---
+++ CurrencyConversionOnStereoids/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+namespace CurrencyConversionOnStereoids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zdejte ticker měny {USD; CZK; EUR} z které chcete převést");
+            string from = Console.ReadLine();
+            Console.WriteLine("Zdejte ticker měny {USD; CZK; EUR} do které chcete převést");
+            string to = Console.ReadLine();
+            //Paralell conversion rate loading
+            GetConversionRate cr = new GetConversionRate(from, to);
+            bool succes = true;
+            Thread t = new Thread(()=> {
+                succes = cr.LoadRate();
+                });
+            t.Start();
+            //Gets amount on main thread
+            decimal amount = GetAmount(from);
+            t.Join();
+            
+            if (!succes)
+            {
+                Console.WriteLine("Něco se nepodařilo, prosím zkuste to později");
+                return;
+            }
+
+            Console.WriteLine($"{amount} {from} je {decimal.Round(amount * cr.GetRate(),2)} {to}");
+
+
+        }
+        static decimal GetAmount(string ticker)
+        {
+            Console.WriteLine($"Zadejte množství ${ticker.ToUpper()}, které chcete převést");
+            decimal am = decimal.Zero;
+            try
+            {
+                am = decimal.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                am = GetAmount(ticker);
+               
+            }
+            return am;
+        }
+    }
+}
